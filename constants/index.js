a024eb029ce045d1a4415ac13f3462f3d0d2cffd
Mamalito <88972054+Mamalito@users.noreply.github.com>
2023-07-24 03:17:12 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x06345aaef44a4fa6a856a2a609b29f9cfe994aaa";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
